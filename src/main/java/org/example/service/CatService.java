4554cb6cfa0bed608c2ff21e4f0e906bf399cdeb
AlsuNuts <forever-nuts@yandex.ru>
2023-03-04 14:11:47 +0300

    Init commit

--- src/main/java/org/example/service/CatService.java
+++ src/main/java/org/example/service/CatService.java
@@ -4,6 +4,7 @@ import org.example.exceptions.CatNotFoundExceprion;
 import org.example.model.Cat;
 import org.example.repository.CatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -25,8 +26,18 @@ public class CatService {
     public void saveCat(Cat newCat){
         repository.save(newCat);
     }
-    public Cat readCat(Long id){
+    public ResponseEntity<Cat> readCat(Long id){
         return repository.findById(id).orElseThrow(()->new CatNotFoundExceprion(id.toString()));
+    }
+    public ResponseEntity<Cat> updateCat(Long id, Cat cat){
+        repository.findById(id).orElseThrow();
+        Cat updateCat = repository.getById(id);
+
+        updateCat.setId(cat.getId());
+        updateCat.setName(cat.getName());
+        updateCat.setWeight(cat.getWeight());
+        updateCat.setAngry(cat.isAngry());
+        return repository.save(updateCat);
     }
     public void deleteCat(Long id){
         repository.deleteById(id);
