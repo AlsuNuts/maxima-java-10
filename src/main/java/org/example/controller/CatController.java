1780b688e3d2b8fe72bebc79aac910b8a1a6d3f9
AlsuNuts <forever-nuts@yandex.ru>
2023-03-04 14:11:59 +0300

    Init commit

--- src/main/java/org/example/controller/CatController.java
+++ src/main/java/org/example/controller/CatController.java
@@ -17,7 +17,7 @@ public class CatController {
     private CatService catService;
     @GetMapping("/cats")
     public List<Cat> getAll(){
-        return catService.qetCats();
+        return catService.getCats();
     }
     @GetMapping("/cat/{id}")
     public ResponseEntity <Cat> getOne(@PathVariable Long id){
@@ -28,4 +28,20 @@ public class CatController {
         catService.saveCat(cat);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
+    @GetMapping("/cat/{id}")
+    public ResponseEntity<Cat> readCat(@PathVariable Long id){
+        return catService.readCat(id);
+    }
+
+
+    @GetMapping("/cat/{id}")
+    public ResponseEntity<Cat> update(@RequestBody Cat cat){
+        return catService.updateCat(cat.getId(), cat);
+    }
+    @GetMapping("/cat/{id}")
+    public void deleteCat(@PathVariable Long id){
+        catService.deleteCat(id);
+   }
+
+
 }
